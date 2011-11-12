4aa32b4e5196198a7297aa1bdc2c1ac5e650335d
gferreira <gustavo@hipertipo.com>
2011-11-12 20:34:27 +0100

    renamed "move layer" dialog to "slide layer", plus a few tweaks and fixes

--- Scripts/current-font/delete-mask.py
+++ Scripts/current-font/delete-mask.py
@@ -1,7 +1,7 @@
-# [h] delete all contents of the mask layer in current font
+# [h] delete mask layer in current font
 
 _layer_name = 'mask'
 
 f = CurrentFont()
 f.removeLayer(_layer_name)
-
+f.update()
---
+++ Scripts/selected-glyphs/slide-layer-dialog.py
@@ -0,0 +1,47 @@
+# [h] slide layer dialog
+
+from vanilla import *
+
+class slideLayerDialog(object):
+
+    _xMax, _xMin = 1400, -1400
+    _yMax, _yMin = 200, -200
+
+    def __init__(self):
+        self._moveX = 0
+        self._moveY = 0
+        self.w = FloatingWindow((260, 150-40), "Slide Layer")
+        # x slider
+        self.w.x_label = TextBox((10, 45-35, -10, 17), "slide x")
+        self.w.x_slider = Slider((65, 45-35, -12, 22), value=0, maxValue=self._xMax, minValue=self._xMin, callback=self.slide)
+        # y slider
+        self.w.y_label = TextBox((10, 75-35, -10, 17), "slide y")
+        self.w.y_slider = Slider((65, 75-35, -12, 22), value=0, maxValue=self._yMax, minValue=self._yMin, callback=self.slide)
+        # flip layers button
+        self.w.button_apply = Button((10, -35, -10, 20), "flip foreground / mask", callback=self.flip_layers_callback)
+        self.w.open()
+
+    def flip_layers_callback(self, sender):
+        font = CurrentFont()
+        _layer_name_1 = 'foreground'
+        _layer_name_2 = 'mask'
+        for gName in font.selection:
+            font[gName].flipLayers(_layer_name_1, _layer_name_2)
+
+    def slide(self, sender):
+        font = CurrentFont()
+        xValue = self.w.x_slider.get()
+        yValue = self.w.y_slider.get()
+        x = self._moveX - xValue
+        y = self._moveY - yValue
+        self._moveX = xValue
+        self._moveY = yValue
+        for gName in font.selection:
+            font[gName].move((x, y))
+
+    def close_callback(self, sender):
+        self.w.close()
+
+
+slideLayerDialog()
+
