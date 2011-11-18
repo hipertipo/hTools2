89da1ee558975d96f5fa3811819abb981143068c
gferreira <gustavo@hipertipo.com>
2011-11-18 21:08:53 +0100

    tweak to the scale glyphs dialog UI, removing padding between buttons

--- Scripts/selected-glyphs/scale-dialog.py
+++ Scripts/selected-glyphs/scale-dialog.py
@@ -6,32 +6,32 @@ class scaleGlyphsDialog(object):
 
     _title = "scale glyphs"
     _button_w = 80
-    _button_h = 30
+    _button_h = 40
     _padding = 10
 
     def __init__(self):
         self.w = FloatingWindow(
-            ((self._button_w*3)+(self._padding*4), (self._button_h*2)+(3*self._padding)),
+            ((self._button_w*3)+(self._padding*2)-2, (self._button_h*2)+(self._padding*2)-1),
             self._title)
         # decrease
         self.w._scale_x_minus_button_1 = SquareButton(
             (self._padding, self._padding, self._button_w, self._button_h),
             "-1%", callback=self._scale_x_minus_1)
         self.w._scale_x_minus_button_10 = SquareButton(
-            (self._button_w+(self._padding*2), self._padding, self._button_w, self._button_h),
+            (self._button_w+((self._padding*1)-1), self._padding, self._button_w, self._button_h),
             "-10%", callback=self._scale_x_minus_10)
         self.w._scale_x_minus_button_100 = SquareButton(
-            ((self._button_w*2)+(self._padding*3), self._padding, self._button_w, self._button_h),
+            ((self._button_w*2)+((self._padding*1)-2), self._padding, self._button_w, self._button_h),
             "-100%", callback=self._scale_x_minus_100)
         # increase
         self.w._scale_x_plus_button_1 = SquareButton(
-            (self._padding, self._button_h+(self._padding*2), self._button_w, self._button_h),
+            (self._padding, self._button_h+(self._padding-1), self._button_w, self._button_h),
             "+1%", callback=self._scale_x_plus_1)
         self.w._scale_x_plus_button_10 = SquareButton(
-            (self._button_w+(self._padding*2), self._button_h+(self._padding*2), self._button_w, self._button_h),
+            (self._button_w+((self._padding*1)-1), self._button_h+(self._padding-1), self._button_w, self._button_h),
             "+10%", callback=self._scale_x_plus_10)
         self.w._scale_x_plus_button_100 = SquareButton(
-            ((self._button_w*2)+(self._padding*3), self._button_h+(self._padding*2), self._button_w, self._button_h),
+            ((self._button_w*2)+(self._padding*1)-2, self._button_h+(self._padding-1), self._button_w, self._button_h),
             "+100%", callback=self._scale_x_plus_100)
         self.w.open()
 
