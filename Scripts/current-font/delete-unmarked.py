cea3c08c80eb90d46231212cc79b23bbc72ea665
gferreira <gustavo@hipertipo.com>
2011-11-24 17:20:28 +0100

    added script: delete unmarked glyphs

---
+++ Scripts/current-font/delete-unmarked.py
@@ -0,0 +1,11 @@
+# [h] delete unmarked glyphs
+
+f = CurrentFont()
+
+print 'deleting unmarked glyphs...\n'
+for g in f:
+    if g.mark == None:
+        print '\tdeleting %s' % g.name
+        f.removeGlyph(g.name)
+f.update()
+print '\n...done.\n'
