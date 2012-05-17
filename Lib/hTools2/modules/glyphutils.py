c953fd3d9a08f223c5a5c4d7fea4a10693213155
gferreira <gustavo@hipertipo.com>
2012-05-17 18:23:33 +0200

    added a dialog to "align layers" script

--- Lib/hTools2/modules/glyphutils.py
+++ Lib/hTools2/modules/glyphutils.py
@@ -159,84 +159,83 @@ def shift_selected_points_x(glyph, delta, anchors=False):
 #---------------
 
 def draw_bounds(g, (x1, y1, x2, y2), (x3, y3)):
-    # x guides
-    g.addGuide((x1, 0), 90, name="x_min")
-    g.addGuide((x2, 0), 90, name="x_max")
-    g.addGuide((x3, 0), 90, name="x_mid")
-    # y guides
-    g.addGuide((0, y1), 0, name="y_min")
-    g.addGuide((0, y2), 0, name="y_max")
-    g.addGuide((0, y3), 0, name="y_mid")
-    # done
-    g.update()
+	# x guides
+	g.addGuide((x1, 0), 90, name="x_min")
+	g.addGuide((x2, 0), 90, name="x_max")
+	g.addGuide((x3, 0), 90, name="x_mid")
+	# y guides
+	g.addGuide((0, y1), 0, name="y_min")
+	g.addGuide((0, y2), 0, name="y_max")
+	g.addGuide((0, y3), 0, name="y_mid")
+	# done
+	g.update()
 
 def get_bounds(g, layer_names):
-    lowest_x = False
-    lowest_y = False
-    highest_x = False
-    highest_y = False
-    for layer_name in layer_names:
-        glyph = g.getLayer(layer_name)
-        if glyph.box is not None:
-            xMin, yMin, xMax, yMax = glyph.box
-            # lowest x
-            if not lowest_x:
-                lowest_x = xMin
-            else:
-                if xMin < lowest_x:
-                   lowest_x = xMin
-            # lowest y            
-            if not lowest_y:
-                lowest_y = yMin
-            else:
-                if yMin < lowest_y:
-                   lowest_y = yMin
-            # highest x
-            if not highest_x:
-                highest_x = xMax
-            else:
-                if xMax > highest_x:
-                    highest_x = xMax
-            # highest y
-            if not highest_y:
-                highest_y = yMax
-            else:
-                if yMax > highest_y:
-                    highest_y = yMax
-    # done
-    return (lowest_x, lowest_y, highest_x, highest_y)
+	lowest_x = False
+	lowest_y = False
+	highest_x = False
+	highest_y = False
+	for layer_name in layer_names:
+		glyph = g.getLayer(layer_name)
+		if glyph.box is not None:
+			xMin, yMin, xMax, yMax = glyph.box
+			# lowest x
+			if not lowest_x:
+				lowest_x = xMin
+			else:
+				if xMin < lowest_x:
+				   lowest_x = xMin
+			# lowest y            
+			if not lowest_y:
+				lowest_y = yMin
+			else:
+				if yMin < lowest_y:
+				   lowest_y = yMin
+			# highest x
+			if not highest_x:
+				highest_x = xMax
+			else:
+				if xMax > highest_x:
+					highest_x = xMax
+			# highest y
+			if not highest_y:
+				highest_y = yMax
+			else:
+				if yMax > highest_y:
+					highest_y = yMax
+	# done
+	return (lowest_x, lowest_y, highest_x, highest_y)
 
 def get_middle((lo_x, lo_y, hi_x, hi_y)):
-    width_all = hi_x - lo_x
-    height_all = hi_y - lo_y
-    middle_x = lo_x + (width_all * .5)
-    middle_y = lo_x + (height_all * .5)
-    return (middle_x, middle_y)
+	width_all = hi_x - lo_x
+	height_all = hi_y - lo_y
+	middle_x = lo_x + (width_all * .5)
+	middle_y = lo_x + (height_all * .5)
+	return (middle_x, middle_y)
 
 def center_layers(g, layer_names, (middle_x, middle_y)):
-    for layer_name in layer_names:
-        glyph = g.getLayer(layer_name)
-        if glyph.box is not None:
-            xMin, yMin, xMax, yMax = glyph.box
-            w = xMax - xMin
-            h = yMax - yMin
-            center_x = xMin + (w * .5)
-            center_y = yMin + (h * .5)
-            shift_x = middle_x - center_x
-            shift_y = middle_y - center_y
-            glyph.move((shift_x, shift_y))
-        g.update()
+	for layer_name in layer_names:
+		glyph = g.getLayer(layer_name)
+		if glyph.box is not None:
+			xMin, yMin, xMax, yMax = glyph.box
+			w = xMax - xMin
+			h = yMax - yMin
+			center_x = xMin + (w * .5)
+			center_y = yMin + (h * .5)
+			shift_x = middle_x - center_x
+			shift_y = middle_y - center_y
+			glyph.move((shift_x, shift_y))
+		g.update()
 
 def center_glyph_layers(g, layers, guides=True):
-    # get center
-    _bounds = get_bounds(g, layers)
-    _middle = get_middle(_bounds)
-    # draw bounds
-    if guides:
-        clear_guides(g)
-        draw_bounds(g, _bounds, _middle)
-    # center layers
-    center_layers(g, layers, _middle)
+	_bounds = get_bounds(g, layers)
+	_middle = get_middle(_bounds)
+	center_layers(g, layers, _middle)
+	if guides:
+		clear_guides(g)
+		_bounds = get_bounds(g, layers)
+		_middle = get_middle(_bounds)
+		draw_bounds(g, _bounds, _middle)
 
 #------------
 # glyph libs
@@ -261,6 +260,6 @@ def clear_glyph_libs(glyph):
 #------------
 
 def clear_guides(glyph):
-    for guide in glyph.guides:
-        glyph.removeGuide(guide)
+	for guide in glyph.guides:
+		glyph.removeGuide(guide)
 	glyph.update()
