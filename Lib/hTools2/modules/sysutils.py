321dfa801ff167f3ba7e0ea80cab68de6cfe1a86
gferreira <gustavo@hipertipo.com>
2012-09-18 02:13:10 +0200

    added RF conditional for shortcut tools

--- Lib/hTools2/modules/sysutils.py
+++ Lib/hTools2/modules/sysutils.py
@@ -2,8 +2,6 @@
 
 import os
 
-from mojo.UI import getScriptingMenuNamingShortKey, setScriptingMenuNamingShortKey
-
 #---------
 # context
 #---------
@@ -33,47 +31,51 @@ def get_context():
 
 _ctx = get_context()
 
-#----------------
-# shortcut tools
-#----------------
+#-------------------------
+# RoboFont shortcut tools
+#-------------------------
 
-def clear_shortcuts():
-    setScriptingMenuNamingShortKey({})
+if _ctx == 'RoboFont':
 
-def print_shortcuts():
-    _dict = getScriptingMenuNamingShortKey()
-    for k in _dict:
-        name, key = _dict[k]
-        print _dict[k][key], _dict[k][name], k, os.path.exists(k)
+    from mojo.UI import getScriptingMenuNamingShortKey, setScriptingMenuNamingShortKey
 
-def set_shortcuts(shortcuts_dict):
-    '''Set RoboFont shortcuts from a dict.'''
-    setScriptingMenuNamingShortKey(shortcuts_dict)
+    def clear_shortcuts():
+        setScriptingMenuNamingShortKey({})
 
-def build_shortcuts_dict(path, shortcuts):
-    #   shortcuts_dict = {
-    #       u'/path/to/script.py': {
-    #           'preferredName' : 'my script',
-    #           'shortKey' : 'n',
-    #       }
-    #   }
-    _shortcuts_dict = {}
-    for shortcut in shortcuts:
-        _key, _name, _file = shortcut
-        _file_path = path + _file
-        if os.path.exists(_file_path):
-            _shortcuts_dict[_file_path] = {}
-            _shortcuts_dict[_file_path]['preferredName'] = _name
-            _shortcuts_dict[_file_path]['shortKey'] = _key
-        else:
-            print '%s does not exist.' % _file_path
-    return _shortcuts_dict
+    def print_shortcuts():
+        _dict = getScriptingMenuNamingShortKey()
+        for k in _dict:
+            name, key = _dict[k]
+            print _dict[k][key], _dict[k][name], k, os.path.exists(k)
 
-def merge_shortcuts_dicts(dicts_list):
-    '''Merge all shortcut dicts from a list.'''
-    _super_dict = {}
-    for _dict in dicts_list:
-        for k in _dict.keys():
-            _super_dict[k] = _dict[k]
-    return _super_dict
+    def set_shortcuts(shortcuts_dict):
+        '''Set RoboFont shortcuts from a dict.'''
+        setScriptingMenuNamingShortKey(shortcuts_dict)
+
+    def build_shortcuts_dict(path, shortcuts):
+        #   shortcuts_dict = {
+        #       u'/path/to/script.py': {
+        #           'preferredName' : 'my script',
+        #           'shortKey' : 'n',
+        #       }
+        #   }
+        _shortcuts_dict = {}
+        for shortcut in shortcuts:
+            _key, _name, _file = shortcut
+            _file_path = path + _file
+            if os.path.exists(_file_path):
+                _shortcuts_dict[_file_path] = {}
+                _shortcuts_dict[_file_path]['preferredName'] = _name
+                _shortcuts_dict[_file_path]['shortKey'] = _key
+            else:
+                print '%s does not exist.' % _file_path
+        return _shortcuts_dict
+
+    def merge_shortcuts_dicts(dicts_list):
+        '''Merge all shortcut dicts from a list.'''
+        _super_dict = {}
+        for _dict in dicts_list:
+            for k in _dict.keys():
+                _super_dict[k] = _dict[k]
+        return _super_dict
 
